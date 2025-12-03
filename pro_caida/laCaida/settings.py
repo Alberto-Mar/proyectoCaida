9dae11d2677ef9a90403dcd618d779d1e3b37d67
Alberto-Mar <albertomarjr5@gmail.com>
2025-12-03 18:15:17 +0100

    feat: 1

--- pro_caida/laCaida/settings.py
+++ pro_caida/laCaida/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql', # Busca las librerias para usar PostgreSQL
-        'NAME': 'bd-caida',
+        'NAME': 'b.d-caida',
         'USER': 'alberto',
         'PASSWORD': '1234',
         'HOST': 'db',    # Nombre del servicio en docker-compose (contenedor)
