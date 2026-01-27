e69d37892a1add805c734427fb604bb50d804127
Alberto-Mar <albertomarjr5@gmail.com>
2026-01-27 13:16:27 +0100

    feat: :art: api y dockerhub

---
+++ pro_caida/scripts_docker/prod/web_migrate.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+postgres_ready() {
+python << END
+import sys
+import os
+import psycopg2
+POSTGRES_DB = open('/run/secrets/POSTGRES_DB', 'r').read().strip()
+POSTGRES_USER = open('/run/secrets/POSTGRES_USER', 'r').read().strip()
+POSTGRES_PASSWORD = open('/run/secrets/POSTGRES_PASSWORD', 'r').read().strip()
+try:
+    conn = psycopg2.connect(dbname=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host="db"
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+until postgres_ready; do
+ >&2 echo "Postgres is unavailable - sleeping"
+ sleep 1
+done
+
+if [ "$MIGRATE" = "yes" ]; then
+  >&2 echo "Postgres is up - migrate "
+  su -m django_user -c "python manage.py migrate --noinput"
+fi
+
+if [ "$STATIC" = "yes" ]; then
+  >&2 echo "Postgres is up - collecting static"
+  su -m django_user -c "python manage.py collectstatic --noinput"
+fi
